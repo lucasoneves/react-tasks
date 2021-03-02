773e894feaede3bb3ce9e3e4025525235e210189
Lucas de Oliveira Neves <LONeves@rd.com.br>
2021-03-02 10:39:25 -0300

    Redux started

---
+++ redux--01-start/redux-basics.js
@@ -0,0 +1,23 @@
+const redux = require('redux');
+
+const createStore = redux.createStore;
+
+const initialState = {
+  counter: 0
+}
+
+// Reducers
+const rootReducer = (state = initialState, action) => {
+  return state;
+}
+
+// Store
+const store = createStore(rootReducer);
+
+console.log(store.getState());
+
+
+
+// Dispatching action
+
+// Subscription
