38601daf7df13de0b3524742d4dd34becde45495
Lucas de Oliveira Neves <LONeves@rd.com.br>
2021-03-02 10:46:43 -0300

    Dispatching actions

--- redux--01-start/redux-basics.js
+++ redux--01-start/redux-basics.js
@@ -8,6 +8,18 @@ const initialState = {
 
 // Reducers
 const rootReducer = (state = initialState, action) => {
+  if (action.type === 'INC_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + 1
+    }
+  }
+  if (action.type === 'ADD_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    }
+  }
   return state;
 }
 
@@ -16,8 +28,9 @@ const store = createStore(rootReducer);
 
 console.log(store.getState());
 
-
-
 // Dispatching action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
 
 // Subscription
