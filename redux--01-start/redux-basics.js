5e9a375e378141c91fe1433f0508a7cc5c4f3fed
Lucas de Oliveira Neves <LONeves@rd.com.br>
2021-03-02 10:51:16 -0300

    Subscription

--- redux--01-start/redux-basics.js
+++ redux--01-start/redux-basics.js
@@ -1,36 +1,38 @@
-const redux = require('redux');
+const redux = require("redux");
 
 const createStore = redux.createStore;
 
 const initialState = {
-  counter: 0
-}
+  counter: 0,
+};
 
 // Reducers
 const rootReducer = (state = initialState, action) => {
-  if (action.type === 'INC_COUNTER') {
+  if (action.type === "INC_COUNTER") {
     return {
       ...state,
-      counter: state.counter + 1
-    }
+      counter: state.counter + 1,
+    };
   }
-  if (action.type === 'ADD_COUNTER') {
+  if (action.type === "ADD_COUNTER") {
     return {
       ...state,
-      counter: state.counter + action.value
-    }
+      counter: state.counter + action.value,
+    };
   }
   return state;
-}
+};
 
 // Store
 const store = createStore(rootReducer);
 
 console.log(store.getState());
+// Subscription
+store.subscribe(() => {
+  console.log("Subscription >>>>>>>> ", store.getState());
+});
 
 // Dispatching action
-store.dispatch({type: 'INC_COUNTER'});
-store.dispatch({type: 'ADD_COUNTER', value: 10});
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
 console.log(store.getState());
-
-// Subscription
